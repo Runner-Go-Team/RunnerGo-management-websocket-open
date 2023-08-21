2fa54270ddf2352c8ddab12db843b0f2158cd9d4
Halooooooo <673574728@qq.com>
2023-08-21 10:24:00 +0800

    redis 集群支持
    redis集群支持

--- internal/init.go
+++ internal/init.go
@@ -20,7 +20,6 @@ func InitProjects(readConfMode int, configFile string) {
 	proof.MustInitProof()
 	//dal.MustInitGRPC()
 	dal.MustInitRedis()
-	dal.MustInitRedisForReport()
 	dal.MustInitBigCache()
 	// 初始化logger
 	zap.S().Debug("初始化logger")
--- internal/pkg/dal/redis.go
+++ internal/pkg/dal/redis.go
@@ -3,18 +3,18 @@ package dal
 import (
 	"github.com/Runner-Go-Team/RunnerGo-management-websocket-open/internal/pkg/conf"
 	"github.com/go-redis/redis/v8"
+	"strings"
 )
 
-var rdb *redis.Client
+var rdb *redis.ClusterClient
 
 func MustInitRedis() {
-	rdb = redis.NewClient(&redis.Options{
-		Addr:     conf.Conf.Redis.Address,
+	rdb = redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:    strings.Split(conf.Conf.Redis.ClusterAddress, ";"),
 		Password: conf.Conf.Redis.Password,
-		DB:       conf.Conf.Redis.DB,
 	})
 }
 
-func GetRDB() *redis.Client {
+func GetRDB() *redis.ClusterClient {
 	return rdb
 }
